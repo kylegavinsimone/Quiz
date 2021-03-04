5a97c25925430997f4041cfdc3b08af4be95c6c1
kylegavinsimone <kylesimone@outlook.com>
2021-03-03 18:49:50 -0700

    CSS and HTML are good, but having issues with Javascript

--- quizscript.js
+++ quizscript.js
@@ -1,148 +1,373 @@
-var quizQuestions = document.getElementById("quiz-questions");
-var timer = document.getElementById("timer");
-var btnStart = document.getElementById("btn-start");
-var timecounter = document.getElementById("timecounter");
-var titleitem = document.getElementById("title-item");
-var nextQuestions;
-var questionanswers = document.getElementById("question-answers");
-var myScore = document.getElementById("score");
-var btnScore = document.getElementById("btnScore");
-var currentindex = 0;
-var score = 0;
-var count = 75;
-var alert = document.getElementById("alert");
-var info = document.getElementById("info");
-// var addscore = document.getElementById("addscore");
-// var submitresult = document.getElementById("submitresult");
-var allScores = [];
-var storedScores = JSON.parse(localStorage.getItem("userData"));
 var questions = [
   {
-    title: "Why did I create this quiz?",
-    choices: ["I always wanted to be a professional quiz maker",
-     "I have a lot of free time on my hands",
-    "So I could learn coding and make fat stacks"],
-    answer: "So I could learn coding and make fat stacks",
-  },
-  {
-    title: "What is the point of this quiz",
-    choices: ["There is no point",
-    "To learn how to cook",
-    "To flex my mad programming skills"],
-    answer: "To flex my mad programming skills",
-  },
-  {
-    title: "How is your day going so far?",
+    question: "1. How do you write 'Hello World' in an alert box?",
     choices: [
-        "Just kidding, I don't care",
-        "Great",
-        "Aweful",
+      "msg('Hello World')",
+      "msgBox('Hello World');",
+      "alertBox('Hello World');",
+      "alert('Hello World');",
     ],
-    answer: "Just kidding, I don't care",
+    correctAnswer: 3,
   },
   {
-    title:
-      "String values must be enclosed within --- when being assigned to variables ",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes",
+    question: "2. How to empty an array in JavaScript?",
+    choices: [
+      "arrayList[]",
+      "arrayList(0)",
+      "arrayList.length=0",
+      "arrayList.len(0)",
+    ],
+    correctAnswer: 2,
   },
   {
-    title:
-      "A very useful tool used during development and debugging for printing content to the debugger is:---",
-    choices: ["JavaScript", "terminal/bash", "alerts", "console.log"],
-    answer: "console.log",
+    question:
+      "3. What function to add an element at the begining of an array and one at the end?",
+    choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
+    correctAnswer: 1,
+  },
+  {
+    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+    choices: ["undefined", "0", "prints nothing", "Syntax error"],
+    correctAnswer: 0,
+  },
+  {
+    question:
+      "5. What would following code return? console.log(typeof typeof 1);",
+    choices: ["string", "number", "Syntax error", "undefined"],
+    correctAnswer: 0,
+  },
+  {
+    question: "6. Which software company developed JavaScript?",
+    choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
+    correctAnswer: 1,
+  },
+  {
+    question: "7. What would be the result of 3+2+'7'?",
+    choices: ["327", "12", "14", "57"],
+    correctAnswer: 3,
+  },
+  {
+    question:
+      "8. Look at the following selector: $('div'). What does it select?",
+    choices: [
+      "The first div element",
+      "The last div element",
+      "All div elements",
+      "Current div element",
+    ],
+    correctAnswer: 2,
+  },
+  {
+    question: "9. How can a value be appended to an array?",
+    choices: [
+      "arr(length).value;",
+      "arr[arr.length]=value;",
+      "arr[]=add(value);",
+      "None of these",
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question:
+      "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
+    choices: ["'32'", "'122'", "'13'", "'14'"],
+    correctAnswer: 0,
   },
 ];
-btnStart.addEventListener("click", starQuiz);
-function starQuiz() {
-  if (storedScores !== null) {
-    allScores = storedScores;
-  }
-  info.classList.add("d-none");
-  btnStart.classList.add("d-none");
-  timecounter.classList.remove("d-none");
-  quizQuestions.classList.remove("d-none");
-  nextQuestions = questions[currentindex];
-  console.log(nextQuestions.title);
 
-  displayQuestion(nextQuestions);
+var currentQuestion = 0;
+var viewingAns = 0;
+var correctAnswers = 0;
+var quizOver = false;
+var iSelectedAnswer = [];
+var c = 180;
+var t;
+$(document).ready(function () {
+  // Display the first question
+  displayCurrentQuestion();
+  $(this).find(".quizMessage").hide();
+  $(this).find(".preButton").attr("disabled", "disabled");
 
-  gametime();
-}
-btnScore.addEventListener("click", function () {
-  let name = document.getElementById("inputScore").value;
-  scorePage(name, count);
+  timedCount();
+
+  $(this)
+    .find(".preButton")
+    .on("click", function () {
+      if (!quizOver) {
+        if (currentQuestion == 0) {
+          return false;
+        }
+
+        if (currentQuestion == 1) {
+          $(".preButton").attr("disabled", "disabled");
+        }
+
+        currentQuestion--; // Since we have already displayed the first question on DOM ready
+        if (currentQuestion < questions.length) {
+          displayCurrentQuestion();
+        }
+      } else {
+        if (viewingAns == 3) {
+          return false;
+        }
+        currentQuestion = 0;
+        viewingAns = 3;
+        viewResults();
+      }
+    });
+
+  // On clicking next, display the next question
+  $(this)
+    .find(".nextButton")
+    .on("click", function () {
+      if (!quizOver) {
+        var val = $("input[type='radio']:checked").val();
+
+        if (val == undefined) {
+          $(document).find(".quizMessage").text("Please select an answer");
+          $(document).find(".quizMessage").show();
+        } else {
+          // TODO: Remove any message -> not sure if this is efficient to call this each time....
+          $(document).find(".quizMessage").hide();
+          if (val == questions[currentQuestion].correctAnswer) {
+            correctAnswers++;
+          }
+          iSelectedAnswer[currentQuestion] = val;
+
+          currentQuestion++; // Since we have already displayed the first question on DOM ready
+          if (currentQuestion >= 1) {
+            $(".preButton").prop("disabled", false);
+          }
+          if (currentQuestion < questions.length) {
+            displayCurrentQuestion();
+          } else {
+            displayScore();
+            $("#iTimeShow").html("Quiz Time Completed!");
+            $("#timer").html(
+              "You scored: " + correctAnswers + " out of: " + questions.length
+            );
+            c = 185;
+            $(document).find(".preButton").text("View Answer");
+            $(document).find(".nextButton").text("Play Again?");
+            quizOver = true;
+            return false;
+          }
+        }
+      } else {
+        // quiz is over and clicked the next button (which now displays 'Play Again?'
+        quizOver = false;
+        $("#iTimeShow").html("Time Remaining:");
+        iSelectedAnswer = [];
+        $(document).find(".nextButton").text("Next Question");
+        $(document).find(".preButton").text("Previous Question");
+        $(".preButton").attr("disabled", "disabled");
+        resetQuiz();
+        viewingAns = 1;
+        displayCurrentQuestion();
+        hideScore();
+      }
+    });
 });
-// Time set
 
-function gametime() {
-  var timeinterval = setInterval(function () {
-    timer.innerText = count;
-    count--;
+function timedCount() {
+  if (c == 185) {
+    return false;
+  }
+
+  var hours = parseInt(c / 3600) % 24;
+  var minutes = parseInt(c / 60) % 60;
+  var seconds = c % 60;
+  var result =
+    (hours < 10 ? "0" + hours : hours) +
+    ":" +
+    (minutes < 10 ? "0" + minutes : minutes) +
+    ":" +
+    (seconds < 10 ? "0" + seconds : seconds);
+  $("#timer").html(result);
+
+  if (c == 0) {
+    displayScore();
+    $("#iTimeShow").html("Quiz Time Completed!");
+    $("#timer").html(
+      "You scored: " + correctAnswers + " out of: " + questions.length
+    );
+    c = 185;
+    $(document).find(".preButton").text("View Answer");
+    $(document).find(".nextButton").text("Play Again?");
+    quizOver = true;
+    return false;
+  }
+
+  /*if(c == 0 )
+		{	
+			if (!quizOver) 
+			{
+				var val = $("input[type='radio']:checked").val();
+            	if (val == questions[currentQuestion].correctAnswer) 
+				{
+					correctAnswers++;
+				}
+				currentQuestion++; // Since we have already displayed the first question on DOM ready
+				
+				if (currentQuestion < questions.length) 
+				{
+					displayCurrentQuestion();
+					c=15;
+				} 
+				else 
+				{
+					displayScore();
+					$('#timer').html('');
+					c=16;
+					$(document).find(".nextButton").text("Play Again?");
+					quizOver = true;
+					return false;
+				}
+			}
+			else 
+			{ // quiz is over and clicked the next button (which now displays 'Play Again?'
+				quizOver = false;
+				$(document).find(".nextButton").text("Next Question");
+				resetQuiz();
+				displayCurrentQuestion();
+				hideScore();
+			}		
+		}	*/
+  c = c - 1;
+  t = setTimeout(function () {
+    timedCount();
   }, 1000);
 }
 
-function scorePage(a, b) {
-  var userData = {
-    inits: a,
-    userScore: b,
-  };
-  allScores.push(userData);
+// This displays the current question AND the choices
+function displayCurrentQuestion() {
+  if (c == 185) {
+    c = 180;
+    timedCount();
+  }
+  //console.log("In display current Question");
+  var question = questions[currentQuestion].question;
+  var questionClass = $(document).find(".quizContainer > .question");
+  var choiceList = $(document).find(".quizContainer > .choiceList");
+  var numChoices = questions[currentQuestion].choices.length;
+  // Set the questionClass text to the current question
+  $(questionClass).text(question);
+  // Remove all current <li> elements (if any)
+  $(choiceList).find("li").remove();
+  var choice;
 
-  localStorage.setItem("userData", JSON.stringify(allScores));
-  location.href = "score.html";
-}
+  for (i = 0; i < numChoices; i++) {
+    choice = questions[currentQuestion].choices[i];
 
-function displayQuestion(question) {
-  titleitem.innerText = question.title;
-  question.choices.forEach((element) => {
-    var button = document.createElement("button");
-    button.className = "btn-primary btn-block text-left";
-    button.innerText = element;
-    // questionanswers.innerHTML=""
-    questionanswers.appendChild(button);
-    button.addEventListener("click", displaynextQuestion);
-  });
-}
-
-function displaynextQuestion(e) {
-  currentindex++;
-  if (currentindex < questions.length) {
-    correction(e.target.innerText == nextQuestions.answer);
-    questionanswers.innerHTML = "";
-    if (currentindex < questions.length) {
-      nextQuestions = questions[currentindex];
-      displayQuestion(nextQuestions);
+    if (iSelectedAnswer[currentQuestion] == i) {
+      $(
+        '<li><input type="radio" class="radio-inline" checked="checked"  value=' +
+          i +
+          ' name="dynradio" />' +
+          " " +
+          choice +
+          "</li>"
+      ).appendTo(choiceList);
     } else {
-      currentindex = 0;
-      displayQuestion(nextQuestions);
+      $(
+        '<li><input type="radio" class="radio-inline" value=' +
+          i +
+          ' name="dynradio" />' +
+          " " +
+          choice +
+          "</li>"
+      ).appendTo(choiceList);
     }
-  } else {
-    console.log("endgame");
-    endgame();
   }
-}
-function correction(response) {
-  if (response) {
-    alert.innerText = "Good";
-    console.log("Good");
-  } else {
-    alert.innerText = "Wrong";
-    count = count - 15;
-    timer.innerHTML = count;
-    console.log("Wrong");
-  }
-  setTimeout(function () {
-    alert.innerText = "";
-  }, 1000);
-}
-function endgame() {
-  // btnStart.classList.add("d-none")
-  myScore.innaText = count;
-  addscore.classList.remove("d-none");
-  timecounter.classList.add("d-none");
-  quizQuestions.classList.add("d-none");
-  addscore.classList.remove("d-none");
 }
 
-submitButton.addEventListener("click", showResults);
+function resetQuiz() {
+  currentQuestion = 0;
+  correctAnswers = 0;
+  hideScore();
+}
+
+function displayScore() {
+  $(document)
+    .find(".quizContainer > .result")
+    .text("You scored: " + correctAnswers + " out of: " + questions.length);
+  $(document).find(".quizContainer > .result").show();
+}
+
+function hideScore() {
+  $(document).find(".result").hide();
+}
+
+// This displays the current question AND the choices
+function viewResults() {
+  if (currentQuestion == 10) {
+    currentQuestion = 0;
+    return false;
+  }
+  if (viewingAns == 1) {
+    return false;
+  }
+
+  hideScore();
+  var question = questions[currentQuestion].question;
+  var questionClass = $(document).find(".quizContainer > .question");
+  var choiceList = $(document).find(".quizContainer > .choiceList");
+  var numChoices = questions[currentQuestion].choices.length;
+  // Set the questionClass text to the current question
+  $(questionClass).text(question);
+  // Remove all current <li> elements (if any)
+  $(choiceList).find("li").remove();
+  var choice;
+
+  for (i = 0; i < numChoices; i++) {
+    choice = questions[currentQuestion].choices[i];
+
+    if (iSelectedAnswer[currentQuestion] == i) {
+      if (questions[currentQuestion].correctAnswer == i) {
+        $(
+          '<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' +
+            i +
+            ' name="dynradio" />' +
+            " " +
+            choice +
+            "</li>"
+        ).appendTo(choiceList);
+      } else {
+        $(
+          '<li style="border:2px solid red;margin-top:10px;"><input type="radio" class="radio-inline" checked="checked"  value=' +
+            i +
+            ' name="dynradio" />' +
+            " " +
+            choice +
+            "</li>"
+        ).appendTo(choiceList);
+      }
+    } else {
+      if (questions[currentQuestion].correctAnswer == i) {
+        $(
+          '<li style="border:2px solid green;margin-top:10px;"><input type="radio" class="radio-inline" value=' +
+            i +
+            ' name="dynradio" />' +
+            " " +
+            choice +
+            "</li>"
+        ).appendTo(choiceList);
+      } else {
+        $(
+          '<li><input type="radio" class="radio-inline" value=' +
+            i +
+            ' name="dynradio" />' +
+            " " +
+            choice +
+            "</li>"
+        ).appendTo(choiceList);
+      }
+    }
+  }
+
+  currentQuestion++;
+
+  setTimeout(function () {
+    viewResults();
+  }, 3000);
+}
